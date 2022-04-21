0888380b6e861c0b6dab31f6ae413c4dbe007cdf
Gleb Bufin <zlarkisz@Zlarkisz.local>
2022-04-21 14:38:44 +0300

    set up project

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,10 +11,65 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/attribute-hyphenation': 'error',
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 1,
+        },
+      },
+    ],
+    'vue/attributes-order': [
+      'error',
+      {
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          ['UNIQUE', 'SLOT'],
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT',
+        ],
+        alphabetical: false,
+      },
+    ],
+    'vue/order-in-components': [
+      'error',
+    ],
+    'vue/new-line-between-multi-line-property': [
+      'error',
+      {
+        minLineOfMultilineProperty: 2,
+      },
+    ],
+    'vue/html-indent': ['error', 2, {
+      attribute: 1,
+      baseIndent: 1,
+      closeBracket: 0,
+      alignAttributesVertically: true,
+      ignores: [],
+    }],
+    'vue/html-closing-bracket-newline': ['error', {
+      singleline: 'never',
+      multiline: 'always',
+    }],
+    'vue/no-reserved-component-names': ['error', {
+      disallowVueBuiltInComponents: true,
+    }],
+    'vue/multi-word-component-names': 0,
+    'vuejs-accessibility/form-control-has-label': 0,
   },
+
   overrides: [
     {
       files: [
