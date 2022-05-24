324776d46c54d2690fbc0697ba4ae136678e4eeb
zlarkisz <blefgufin@gmail.com>
2022-05-24 21:36:16 +0300

    added deploy

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,17 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/photo-albums-Vue3-TS-Tests/'
+    : '/',
   transpileDependencies: true,
-  // css: {
-  //   loaderOptions: {
-  //     scss: {
-  //       additionalData: '@import "@/assets/styles/global.scss";',
-  // eslint-disable-next-line global-require
-  //       implementation: require('node-sass'),
-  //     },
-  //   },
-  // },
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: '@import "@/assets/styles/global.scss";',
+        // eslint-disable-next-line global-require
+        //       implementation: require('node-sass'),
+      },
+    },
+  },
 });
